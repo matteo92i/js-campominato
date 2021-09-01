cc443d75ea4035eba3a22f7c070074ebb78d154a
Matteo92i <matteo.iovane@gmail.com>
2021-09-01 17:00:34 +0200

    lista16 numeri

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function randomNumber(uno, sedici) {
+    var result = Math.floor(Math.random() * 100)
+}
+    
+    
+
+let listaNumeriPC = [];
+
+var random = randomNumber(1, 100)
+for (var i = 0; i < 16; i++) {
+    var resultRandom16 = randomNumber(1, 100);
+    listaNumeriPC.push(resultRandom16)
+  
+}
+console.log(listaNumeriPC)
